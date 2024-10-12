4aeb01e33c8467a83b6f10432d4e317df42af186
Samuel Frederico <sfrederico@ucs.br>
2024-10-12 15:19:14 -0300

    feat: :sparkles: Add 'Computador', 'Bloco', and 'Sala' models to admin site

--- app/lotus/admin.py
+++ app/lotus/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from lotus.models import Bloco, Computador, Sala
+
 # Register your models here.
+admin.site.register(Computador)
+admin.site.register(Bloco)
+admin.site.register(Sala)
