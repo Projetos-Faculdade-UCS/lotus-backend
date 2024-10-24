dbe1977ad0f6870b2c0c16048793d2e137df770b
Samuel Frederico <sfrederico@ucs.br>
2024-10-23 21:54:05 -0300

    feat: :sparkles: Add 'LicencaSoftware' and 'Programa' models and update admin.py

--- app/lotus/models/__init__.py
+++ app/lotus/models/__init__.py
@@ -1,4 +1,14 @@
 from lotus.models.ativos_ti import AtivoTI, Computador, Impressora, Monitor
+from lotus.models.computador_relations import LicencaSoftware, Programa
 from lotus.models.locais import Bloco, Sala
 
-__all__ = ["Bloco", "Sala", "AtivoTI", "Computador", "Impressora", "Monitor"]
+__all__ = [
+    "AtivoTI",
+    "Computador",
+    "Impressora",
+    "Monitor",
+    "Bloco",
+    "Sala",
+    "LicencaSoftware",
+    "Programa",
+]
