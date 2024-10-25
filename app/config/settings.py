768876dbc34df39d5732ec695b296baac97c1baa
Samuel Frederico <sfrederico@ucs.br>
2024-10-25 17:25:15 -0300

    feat: :beers: Needed modifications to present MileStone-1

--- app/config/settings.py
+++ app/config/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
 
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 ./app/manage.py runserver 0.0.0.0:8000
