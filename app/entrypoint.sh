623bd5d95c01ec252ad4b37eb6f3f0640166606d
Marco Haubrich <marcolino.haubrich@gmail.com>
2024-12-03 02:21:08 -0300

    feat: :sparkles: Update Ruff configuration, enhance Docker health checks, and improve entrypoint script logic

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # wait for db
-sleep 10
 python3 manage.py makemigrations
 python3 manage.py migrate
 
 # create superuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', '123123')" | python3 manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', '123123')" | python3 manage.py shell
 
 python3 manage.py runserver 0.0.0.0:8000
